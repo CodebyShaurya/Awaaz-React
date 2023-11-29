0a2e4162fd8763e3fea3bd79df7417744d1c3939
Vinay Gupta <shauryagupta118@gmail.com>
2023-11-29 22:04:16 +0530

    dgr

--- src/Components/Learnings.jsx
+++ src/Components/Learnings.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './Nav';
 import '../CSS/Learnings.css';
 import Footer from './Footer';
+import { ReactMic } from 'react-mic';
 
 // const [wordData, setWordData] = useState(null);
 
@@ -23,11 +24,52 @@ import Footer from './Footer';
 //     });
 
 
-const Learnings = (data) => {
-    const [wordData, setWordData] = useState(null);
-    const [listen, setListen] = useState('Listen');
-    const [mic, setMic] = useState('OFF');
-    const fetchData = () => {
+class Learnings extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        record: false,
+        mic:'OFF',
+        listen:'Listen',
+        wordData:null
+
+      }
+    }
+   
+    startRecording = () => {
+      this.setState({ record: true });
+    }
+   
+    stopRecording = () => {
+      this.setState({ record: false });
+    }
+   
+    onData(recordedBlob) {
+    //   console.log('chunk of real-time data is: ', recordedBlob);
+    }
+   
+    onStop(recordedBlob) {
+      console.log('recordedBlob is: ', recordedBlob);
+      fetch('http://localhost:5000/save_audio', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ 'URL':recordedBlob.blobURL } ) } ) .then(response => {
+                if (!response.ok) {
+                  throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.text();
+              })
+              .then(data => {
+                console.log(data); // The response from the server
+              })
+              .catch(error => {
+                console.error('Error:', error);
+              });
+            } 
+
+    fetchData = () => {
         fetch('http://localhost:5000/generate_word', {
             method: 'GET',
         })
@@ -38,15 +80,16 @@ const Learnings = (data) => {
                 return response.json();
             })
             .then(data => {
-                setWordData(data);
+                this.setState({wordData:data});
                 console.log(data);
             })
             .catch(error => {
                 console.error('Problem detected', error);
             });
-    };
-    return (
-
+    }
+   
+    render() {
+      return (
         <>
             <Nav />
 
@@ -54,9 +97,9 @@ const Learnings = (data) => {
 
                 <div className="main2">Detection</div>
                 <div className="main3">
-                    <button onClick={fetchData} style={{ position: "absolute", left: "550px", top: "250px", borderRadius: "8px", height: "2.5rem", background: "none", borderColor: "white", color: "white" }}>Generate New Word</button>
-                    {wordData ? (
-                        <div className="main4">{wordData.word1 ?? 'Test Yourself'}<br />{wordData.pronounciation}</div>
+                    <button onClick={this.fetchData} style={{ position: "absolute", left: "550px", top: "250px", borderRadius: "8px", height: "2.5rem", background: "none", borderColor: "white", color: "white" }}>Generate New Word</button>
+                    {this.state.wordData ? (
+                        <div className="main4">{this.state.wordData.word1 ?? 'Test Yourself'}<br />{this.state.wordData.pronounciation}</div>
                     ) : (
                         <div className="main4" style={{ top: "8rem" }}>Let's test</div>
 
@@ -68,21 +111,23 @@ const Learnings = (data) => {
                         4. Shunday (002%)<br />
                         5. Shundae (002%)
                     </div>
-                    <div className="main6" style={{ borderColor: mic === 'ON' ? 'red' : 'white', borderWidth:'10px' }}>
+                    <div className="main6" style={{ borderColor: this.state.record === true ? 'red' : 'white', borderWidth:'10px' }}>
                         <svg style={{ position: 'absolute', left: '40', top: '35' }} xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="white" class="bi bi-mic" viewBox="0 0 16 16">
                             <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5" />
                             <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3" />
                         </svg>
                     </div>
                     <div className="main7" onClick={() => {
-                        if(mic == 'OFF') {
-                            setMic('ON');
-                            setListen('Listening');
-                        } else if(mic == 'ON') {
-                            setMic('OFF');
-                            setListen('Listen');
+                        if(!this.state.record ) {
+                            this.setState({ mic : 'ON' })
+                            this.setState({ record: true });
+                            this.setState({ listen : 'Listening' })
+                        } else  {
+                            this.setState({ mic : 'OFF' })
+                            this.setState({ record: false });
+                            this.setState({ listen : 'Listen' })
                         }
-                    }}>{listen}</div>
+                    }}>{this.state.listen}</div>
                     <div className="main8" style={{ top: '20rem', left: "31rem" }}>Average % approved = 40%</div>
                     <div className="main9">Attempts</div>
                 </div>
@@ -132,8 +177,26 @@ const Learnings = (data) => {
                 </div>
             </div> <br /> <br />
             <Footer />
+        
+        <div className='qwertyui'>
+          <ReactMic
+            record={this.state.record}
+            className="sound-wave qwertyui"
+            onStop={this.onStop}
+            onData={this.onData}
+            strokeColor="#000000"
+            backgroundColor="#FF4081" />
+          {/* <button onClick={this.startRecording} type="button">Start</button>
+          <button onClick={this.stopRecording} type="button">Stop</button> */}
+
+        </div>
         </>
-    );
-}
+      );
+    }
+  }
+
+
+
+
 
 export default Learnings;
